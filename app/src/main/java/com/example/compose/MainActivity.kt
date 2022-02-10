11275f295a62877e2068cf7dae65fba91c551d15
Ypypy <kkkmit@mail.ru>
2022-02-10 16:10:36 +0300

    modifier.Behevior

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.example.compose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -18,6 +19,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,9 +30,21 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column() {
+            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
              ListItem(name = "yea", prof ="p" )
              ListItem(name = "star", prof ="fin" )
+             ListItem(name = "star", prof ="fin" )
+             ListItem(name = "star", prof ="fin" )
+             ListItem(name = "star", prof ="fin" )
+             ListItem(name = "star", prof ="fin" )
+             ListItem(name = "star", prof ="fin" )
+             ListItem(name = "sd", prof ="sd" )
+             ListItem(name = "sd", prof ="sd" )
+             ListItem(name = "sd", prof ="sd" )
+             ListItem(name = "sd", prof ="sd" )
+             ListItem(name = "sd", prof ="sd" )
+             ListItem(name = "sd", prof ="sd" )
+             ListItem(name = "sd", prof ="sd" )
              ListItem(name = "sd", prof ="sd" )
 
             }
@@ -43,6 +57,11 @@ private fun ListItem(name: String,prof: String){
     Card(
         modifier = Modifier
             .fillMaxWidth()
+            .pointerInput(Unit){
+                detectDragGesturesAfterLongPress { change, dragAmount ->
+                    Log.d("MyLog","LONG")
+                }
+            }
             .padding(10.dp),
         shape = RoundedCornerShape(15.dp),
         elevation = 5.dp
